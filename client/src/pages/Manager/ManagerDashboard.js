11b283cf7d0fd36d9a7e4df9e871c3e49b38c223
FaizAhmad0 <118246870+FaizAhmad0@users.noreply.github.com>
2025-02-09 17:07:42 +0530

    update website managerdash

--- client/src/pages/Manager/ManagerDashboard.js
+++ client/src/pages/Manager/ManagerDashboard.js
@@ -115,7 +115,7 @@ const ManagerDashboard = () => {
             ),
           },
         ]
-      : service === "WEBSITE"
+      : service === "Website"
       ? [
           {
             label: "Website",
